e89aff0eee90dd7ae7aab2ea1c6034f07ee1b7f7
kevinmadrizbrenes <kevinmadrizbrenes@gmail.com>
2025-04-03 20:35:26 -0600

    Second Commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from '../components/header';
+import Header from '../components/HeaderTemp';
 import DataList from '../components/DataList';
 
 const Home = () => {
